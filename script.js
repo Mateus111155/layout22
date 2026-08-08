04356ac2b4cda8b44c8639529a0fe2426684b794
Mateus111155 <mateusfelipe.batista@hotmail.com>
2026-08-08 14:39:24 -0300

    Add cart functionality to update quantity and total

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+let quantidade = 0;
+let total = 0;
 
+function adicionarCarrinho(preco) {
+
+    quantidade++;
+
+    total = total + preco;
+
+    document.getElementById("quantidade").textContent = quantidade;
+
+    document.getElementById("total").textContent = total;
+
+}
